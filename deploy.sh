010286db8426633a39c5d64d3dde16e8f1e14f11
CaiXing0808 <caixing0808@gmail.com>
2019-05-03 15:36:53 -0400

    config

--- deploy.sh
+++ deploy.sh
@@ -12,9 +12,9 @@ USERNAME=caixing0808
 SOURCE=hugo
 # The commit message.
 MESSAGE="Site rebuild $(date)"
-msg() {
-    printf "\033[1;32m :: %s\n\033[0m" "$1"
-}
+#msg() {
+#    printf "\033[1;32m :: %s\n\033[0m" "$1"
+#}
 msg "Pulling down the \`master\` branch into \`public\` to help avoid merge conflicts"
 git subtree pull --prefix=public \
     git@github.com:$USERNAME/$USERNAME.github.io.git origin master -m "Merge origin master"
